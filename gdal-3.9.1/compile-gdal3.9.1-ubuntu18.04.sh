dc488925dc966ca8ee41ee9314816f494cb43cf6
我下班了蟹老板 <2530304058@qq.com>
2024-08-07 17:46:47 +0800

    Deleting cache reduces the size of Docker images and minimizes system disk usage.

--- gdal-3.9.1/compile-gdal3.9.1-ubuntu18.04.sh
+++ gdal-3.9.1/compile-gdal3.9.1-ubuntu18.04.sh
@@ -125,8 +125,7 @@ cmake -DFileGDB_INCLUDE_DIR=/usr/include/ -DPostgreSQL_LIBRARY=/usr/local/pgsql/
 -DPostgreSQL_INCLUDE_DIR=/usr/local/pgsql/include/ -DCMAKE_BUILD_TYPE=Release \
 -DJSON-C_INCLUDE_DIR=/usr/local/json-c-0.15/include \
 -DJSON-C_LIBRARY=/usr/local/json-c-0.15/lib/libjson-c.so .. && cmake --build . && cmake --build . --target install &&
-cp swig/java/libgdalalljni.so /usr/lib && rm -rf /gdal391_compiler/packages &&
-rm -rf /gdal391_compiler
+cp swig/java/libgdalalljni.so /usr/lib && rm -rf /gdal391_compiler
 
 gdalinfo --formats
 gdalinfo --version
